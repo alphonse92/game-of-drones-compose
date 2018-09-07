e142d164bb90b7b563c43ab4c2885ef5e42585aa
Alejandro Molina <alejandromolina@kubesoft.com>
2018-09-07 18:59:50 -0500

    first commit with install file and start file

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+        git submodule add $url $path
+    done
+
+git submodule update --init --recursive
+git submodule foreach --recursive git fetch
+git submodule foreach git merge origin master
+
+cd game-of-drones-client
+npm install .
+cd ..
+cd game-of-drones-api
+npm install .
+cd ..
+
+docker-compose build api

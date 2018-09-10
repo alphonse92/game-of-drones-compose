99582e1de7b421385c62df15fe0bb0dd6efe85c0
Alejandro Molina <alejandromolina@kubesoft.com>
2018-09-09 19:02:55 -0500

    updating install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 set -e
-git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-    while read path_key path
-    do
-        url_key=$(echo $path_key | sed 's/\.path/.url/')
-        url=$(git config -f .gitmodules --get "$url_key")
-        git submodule add $url $path
-    done
-
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
 git submodule foreach git merge origin master
